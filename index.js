5ce5009d1567c8bf7ac49c1b2e2a090665c955df
David J. Davis <davidjamesdavis.djd@gmail.com>
2016-11-16 09:15:36 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
-var Papa = require('babyparse');
-var request = require('request');
-var fs = require('fs');
 
 app.use(express.static(__dirname + '/public'));
 
